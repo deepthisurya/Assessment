9a5ad5957f9d1949e55a3c0eca4e14aa0fe1522d
Deepthi vemparala <deepthisurya.v@gmail.com>
2021-01-25 17:29:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,60 +1,15 @@
-const http = require('http');
 const express = require('express');
+const randomListRouter = require('./routes/randomListRouter')();
 
 const app = express();
-const routerHandle = express.Router();
-//const hostname = "127.0.0.1";
 const port = process.env.port || 3000;
 
-routerHandle.route('/getRandomNumber/:id')
-    .get((req, res) => {
 
-        /* const response = {
-            hello: 'Hey this it the response'
-        };
-        res.json(response); */
-        let getRandom = getRandomNumber(req.params.id);
-        res.send({ number: getRandom });
-    });
-routerHandle.route('/getRandomNumber')
-    .get((req, res) => {
-        let getRandom = getRandomNumber(2);
-        res.send({ number: getRandom });
-    });
-
-
-app.use('/api', routerHandle);
-
+app.use('/api', randomListRouter);
 app.get('/', (req, res) => {
-    res.send('Hello Akshay!!!');
+    res.send('Hi tongadive! Lets generate Random Numbers....');
 });
-
-function getRandomNumber(k) {
-    let randomNum = [];
-    for (let i = 0; i < k; i++) {
-        let random = Math.floor(Math.random() * 10000000000000);
-        randomNum.push(random);
-    }
-    //console.log(randomNum);
-    return randomNum;
-}
-
 
 app.listen(port, () => {
     console.log(`Running at ${port}`);
 });
-
-//npm run start
-
-/* 
-const server = http.createServer((req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-}); */
